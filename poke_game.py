0c895c249e8efd7e97c8560b4d0943f1c52497da
Neha Bhomia <nehabhomia@hotmail.co.uk>
2018-12-05 11:14:39 -0500

    created file for the game code

---
+++ poke_game.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  5 11:07:45 2018
+
+@author: nehabhomia
+"""
+
+from poke_db import *
+
+# =============================================================================
+# Classes
+# =============================================================================
+
+class Trainer:
+    '''
+    Will create the instance of a trainer.
+    input = trainer name, choice of region
+    attributes = name, region
+    '''
+    
+class Pokemon:
+    '''
+    Will create the instance of a pokemon.
+    input = trainer's choice of region
+    attributes = pokemon name, type, region, moves, health
+    '''
+
+# =============================================================================
+# Functions
+# =============================================================================
