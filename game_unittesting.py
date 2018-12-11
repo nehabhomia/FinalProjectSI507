e79785382702bb59b1b0cb659c80b522a71eb7ba
Neha Bhomia <nehabhomia@hotmail.co.uk>
2018-12-10 23:43:25 -0500

    added skeleton for unit testing

---
+++ game_unittesting.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 10 23:35:10 2018
+
+@author: nehabhomia
+"""
+
+import unittest
+import poke_game as game
+
+class TestTrainer(unittest.TestCase):
+
+    def testConstructor(self):
+        pass
+    
+class TestPokemon(unittest.TestCase):
+    
+    def testConstructor(self):
+        pass
+
+unittest.main()
